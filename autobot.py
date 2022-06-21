bcac45a1488914548cf0a3d91a3f7bfaa004660f
koyu <me@koyu.space>
2022-06-21 12:13:21 +0200

    Fix wordlist parser

--- autobot.py
+++ autobot.py
@@ -14,8 +14,10 @@ f.close()
 # Parse wordlist
 wordlist = []
 for l in s:
-  if not l.startswith("#") or not l == "" or not l == "\n":
-      wordlist.append(l.replace("\n", ""))
+  if not l.startswith("#"):
+    if not l == "":
+      if not l == "\n":
+        wordlist.append(l.replace("\n", ""))
 
 # Do the actual work
 for item in wordlist:
